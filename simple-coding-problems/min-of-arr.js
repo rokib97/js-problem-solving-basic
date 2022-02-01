2dd08211b4a3c5c68aa661479c4786d97159c6a4
rokib97 <hasanrokib1997@gmail.com>
2022-02-01 17:51:41 +0600

    added new solve

---
+++ simple-coding-problems/min-of-arr.js
@@ -0,0 +1,18 @@
+/* ১. একটা কোড লিখো। যেটা দিয়ে কোন একটা array এর মধ্যে সবচেয়ে ছোট সংখ্যা বের করে দিতে পারবে।  */
+
+function smallElementOfArr(input) {
+    let smallestNum = input[0];
+    for(let i =1; i<input.length; i++){
+        let arrElement = input[i];
+        if(arrElement < smallestNum){
+            smallestNum = arrElement;
+        }
+
+    }
+    return smallestNum;
+    
+}
+
+let myArr = [52, 10, 20, 4, 77, 120,2]
+let mySmallArrElement = smallElementOfArr(myArr);
+console.log(mySmallArrElement);
